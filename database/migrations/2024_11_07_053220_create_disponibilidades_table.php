9c7700bef75710bd90799f630e4d5ca3059f6f07
valentinaAlvarez11 <valentina.alvarezi@autonoma.edu.co>
2024-11-07 00:33:34 -0500

    Creación de la migración disponibilidades con su estructura

---
+++ database/migrations/2024_11_07_053220_create_disponibilidades_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disponibilidades', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('veterinario_id')->constrained('veterinarios')->onDelete('cascade');
+            $table->date('fecha');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disponibilidades');
+    }
+};
