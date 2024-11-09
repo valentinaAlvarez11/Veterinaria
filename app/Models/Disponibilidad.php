fb5b3b7ce71b50b673674739870bfb0b62f9ef6d
valentinaAlvarez11 <valentina.alvarezi@autonoma.edu.co>
2024-11-09 17:41:04 -0500

    Creación modelo Disponibilidad

---
+++ app/Models/Disponibilidad.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Disponibilidad extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'veterinario_id', 'dia', 'hora_inicio', 'hora_fin'
+    ];
+
+    protected $casts = [
+        'hora_inicio' => 'time',
+        'hora_fin' => 'time',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function veterinario()
+    {
+        return $this->belongsTo(Veterinario::class);
+    }
+}
