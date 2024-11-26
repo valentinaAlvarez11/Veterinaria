bddc2347080d0879f624ce98d3b61511c3695d54
valentinaAlvarez11 <valentina.alvarezi@autonoma.edu.co>
2024-11-26 16:30:40 -0500

    Cambio en el modelo veterinaria

---
+++ app/Models/Veterinarian.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Veterinarian extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'veterinarians';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'specialty',
+        'email',
+        'phone',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
